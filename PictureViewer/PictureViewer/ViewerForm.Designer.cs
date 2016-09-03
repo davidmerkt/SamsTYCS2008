71c6e836bc8f3f1be3786b7fcee3bf8ce41f67a5
David Merkt <davidmerkt@users.noreply.github.com>
2016-09-03 13:08:06 -0400

    Chapter 01

---
+++ PictureViewer/PictureViewer/ViewerForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PictureViewer
+{
+    partial class ViewerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewerForm));
+            this.btnSelectPicture = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.picShowPicture = new System.Windows.Forms.PictureBox();
+            this.ofdSelectPicture = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picShowPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectPicture
+            // 
+            this.btnSelectPicture.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectPicture.Name = "btnSelectPicture";
+            this.btnSelectPicture.Size = new System.Drawing.Size(85, 23);
+            this.btnSelectPicture.TabIndex = 0;
+            this.btnSelectPicture.Text = "Select Picture";
+            this.btnSelectPicture.UseVisualStyleBackColor = true;
+            this.btnSelectPicture.Click += new System.EventHandler(this.btnSelectPicture_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnQuit.Location = new System.Drawing.Point(297, 12);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // picShowPicture
+            // 
+            this.picShowPicture.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picShowPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picShowPicture.Location = new System.Drawing.Point(12, 41);
+            this.picShowPicture.Name = "picShowPicture";
+            this.picShowPicture.Size = new System.Drawing.Size(360, 233);
+            this.picShowPicture.TabIndex = 2;
+            this.picShowPicture.TabStop = false;
+            // 
+            // ofdSelectPicture
+            // 
+            this.ofdSelectPicture.Filter = "Bitmaps|*.BMP|JPEG Images|*.JPG; *.JPEG|GIF Images|*.GIF|PNG Images|*.PNG|TIFF Im" +
+    "ages|*.TIF; *.TIFF";
+            this.ofdSelectPicture.Title = "Select Picture";
+            // 
+            // ViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 286);
+            this.Controls.Add(this.picShowPicture);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnSelectPicture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ViewerForm";
+            this.Text = "Picture Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.picShowPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectPicture;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.PictureBox picShowPicture;
+        private System.Windows.Forms.OpenFileDialog ofdSelectPicture;
+    }
+}
+
---
+++ PictureViewer/PictureViewer/ViewerForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class ViewerForm : Form
+    {
+        public ViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectPicture_Click(object sender, EventArgs e)
+        {
+            if (ofdSelectPicture.ShowDialog() == DialogResult.OK)
+            {
+                picShowPicture.Image = Image.FromFile(ofdSelectPicture.FileName);
+                this.Text = String.Concat("Picture Viewer (" + ofdSelectPicture.FileName + ")");
+                ofdSelectPicture.FileName = "";
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
